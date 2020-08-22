25cffa25ddbfd5c45deaaf3bafba31a112d1624e
Bryan Monzón <bryan.bmmf@gmail.com>
2020-08-22 16:04:48 -0600

    Estructura inicial

---
+++ Funciones.java
@@ -0,0 +1,34 @@
+public class Funciones {
+
+    public static void main(String args[], int argv) {
+
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+        
+        while(!salir){
+            System.out.println("1. Opcion 1");
+            System.out.println("2. Opcion 2");
+            System.out.println("3. Opcion 3");
+            System.out.println("4. Salir");
+            
+            System.out.println("Escribe una de las opciones");
+            opcion = sn.nextInt();
+
+            switch (opcion){
+
+                case 1:
+
+                break;
+
+                default :
+                    
+
+            }
+            
+        }
+
+
+    }
+
+}
