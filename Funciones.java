a7747b7f651f950f99845e8868d9cb31901533e8
Jonathan Chiroy <jonychiroy_9@hotmail.com>
2020-08-22 16:23:31 -0600

    Se agrego la funcion para calcular raiz cubica

--- Funciones.java
+++ Funciones.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Funciones {
 
     public static void main(String args[], int argv) {
@@ -5,13 +7,13 @@ public class Funciones {
         Scanner sn = new Scanner(System.in);
         boolean salir = false;
         int opcion; //Guardaremos la opcion del usuario
-        
+
         while(!salir){
             System.out.println("1. Opcion 1");
             System.out.println("2. Opcion 2");
             System.out.println("3. Opcion 3");
             System.out.println("4. Salir");
-            
+
             System.out.println("Escribe una de las opciones");
             opcion = sn.nextInt();
 
@@ -22,13 +24,21 @@ public class Funciones {
                 break;
 
                 default :
-                    
+
 
             }
-            
+
         }
 
 
     }
+
+    /* Funcion para calcular la raiz cubica */
+    public static void calcularRaizCubica(double valor){
+      double  resultado;
+       Scanner sc=new Scanner(System.in);
+       resultado=Math.pow(valor,1.0/3.0);
+       System.out.println("Su raiz cubica es:"+resultado);
+     }
 
 }
